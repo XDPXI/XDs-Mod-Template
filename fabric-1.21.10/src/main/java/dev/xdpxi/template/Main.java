2ee312c893cde00ff0ebbf2193d063631fd53709
Xavier Perri <contact@xdpxi.dev>
2026-02-10 13:00:55 +1100

    Renamings

--- fabric-1.21.10/src/main/java/dev/xdpxi/template/Main.java
+++ fabric-1.21.10/src/main/java/dev/xdpxi/template/Main.java
@@ -7,16 +7,10 @@ public class Main implements ModInitializer {
 
     @Override
     public void onInitialize() {
-        Log.info("[XDLib] - Initializing...");
+        Log.info("[Template] - Initializing...");
 
         Common.init();
 
-        Log.info("[XDLib] - Testing register API (v6)...");
-        dev.xdpxi.template.api.v6.Register.init();
-
-        Log.info("[XDLib] - Testing register API (v7)...");
-        dev.xdpxi.template.api.v7.Register.init();
-
-        Log.info("[XDLib] - Initialized successfully!");
+        Log.info("[Template] - Initialized successfully!");
     }
 }
